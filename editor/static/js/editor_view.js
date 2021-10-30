9060e5efefd08ddb70668f854762f3001a3172d2
Swanand01 <swanandmathekar@gmail.com>
2021-10-31 00:56:51 +0530

    Added editor to section view

--- editor/static/js/editor_view.js
+++ editor/static/js/editor_view.js
@@ -1,5 +1,5 @@
 let editor;
-
+let form = document.querySelector('#form');
 ClassicEditor
     .create(document.querySelector('#editor'))
     .then(newEditor => {
@@ -10,22 +10,15 @@ ClassicEditor
     });
 
 // Assuming there is a <button id="submit">Submit</button> in your application.
-document.querySelector('h1').addEventListener('click', () => {
+form.addEventListener('submit', (event) => {
     const editorData = editor.getData();
-
-    console.log(typeof (editorData));
+    document.querySelector('#content').value = editorData;
+    //form.submit();
+    
 });
 
 
-document.querySelector("#submit1").addEventListener("click", event => {
-    const editorData = editor.getData();
-    document.querySelector("#content1").value = editorData;
-});
-
-document.querySelector("#addChapter-btn").addEventListener("click", event => {
-    console.log("Done");
-    // let chapter = document.createElement("div");
-    // chapter.className = "accordion-item";
-    // chapter.innerHTML = `{% include 'chapter.html' %}`
-    // document.querySelector(".chapters-list").appendChild(chapter)
-});
+// document.querySelector("#submit1").addEventListener("click", event => {
+//     const editorData = editor.getData();
+//     document.querySelector("#content1").value = editorData;
+// });
